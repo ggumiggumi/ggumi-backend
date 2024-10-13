e90ec842d9f9e07e47ef456d8a8d5e03e81e7716
Jaehyun <skwent77@gmail.com>
2024-10-13 13:17:09 +0900

    feat: project setting

---
+++ src/main/java/com/uplus/ggumi/GgumiApplication.java
@@ -0,0 +1,13 @@
+package com.uplus.ggumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgumiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uplus/ggumi/GgumiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uplus.ggumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgumiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
