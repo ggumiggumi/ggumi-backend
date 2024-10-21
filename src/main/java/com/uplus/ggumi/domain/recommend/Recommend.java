133f4972899819413f817af651e7e5ea3bb3e71a
hyoseok <alexization@gmail.com>
2024-10-21 17:16:11 +0900

    feat: Entity 테이블 추가 및 필드 추가

---
+++ src/main/java/com/uplus/ggumi/domain/recommend/Recommend.java
@@ -0,0 +1,30 @@
+package com.uplus.ggumi.domain.recommend;
+
+import com.uplus.ggumi.domain.book.Book;
+import com.uplus.ggumi.domain.child.Child;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Recommend {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "child_id")
+    private Child child;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+}
