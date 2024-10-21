1ba19907f9b9992a1689f03079a6867c3a1215dc
hyoseok <alexization@gmail.com>
2024-10-21 19:23:03 +0900

    feat(UR-71): 피드백 취소 API 보완후 추가 및 총 좋아요 수 증감 Redis 연결

--- src/main/java/com/uplus/ggumi/service/BookDetailService.java
+++ src/main/java/com/uplus/ggumi/service/BookDetailService.java
@@ -14,16 +14,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class BookDetailService implements BookDetailRepository {
 
+    public static final int TTL = 1;
+    private static final String TOTAL_LIKES = "totalLikes:";
     private static final String LIKE = "like:";
     private static final String HATE = "hate:";
-
     private static final Double LEARNING_RATE = 0.15;
-
     private final RedisTemplate<String, Object> redisTemplate;
 
     private final BookRepository bookRepository;
@@ -53,6 +56,7 @@ public class BookDetailService implements BookDetailRepository {
             redisTemplate.opsForSet().add(HATE + bookId, childId.toString());
         }
 
+        /* 도서 상세 페이지에 필요한 데이터 */
         return BookDetailResponseDto.builder()
                 .title(book.getTitle())
                 .author(book.getAuthor())
@@ -60,15 +64,29 @@ public class BookDetailService implements BookDetailRepository {
                 .bookImage(book.getBook_image())
                 .createdAt(book.getCreatedAt())
                 .publisher(book.getPublisher())
+                .likes(getTotalLikes(book, bookId))
                 .feedback(feedback.getThumbs())
                 .build();
+    }
+
+    /* 해당 도서의 총 좋아요 수에 대한 Cache Aside 작업 */
+    private Integer getTotalLikes(Book book, Long bookId) {
+        Object cachedLikes = redisTemplate.opsForValue().get(TOTAL_LIKES + bookId);
+        if (cachedLikes != null) {
+            return Integer.parseInt(cachedLikes.toString());
+        }
+
+        redisTemplate.opsForValue().set(TOTAL_LIKES + bookId, String.valueOf(book.getLikes()), TTL, TimeUnit.HOURS);
+
+        return book.getLikes();
     }
 
     @Override
     public Long setLike(Long key, Long value) {
         /* 싫어요에 대한 기록을 지워줌 */
         redisTemplate.opsForSet().remove(HATE + key.toString(), value.toString());
-
+        /* 총 좋아요 개수 +1 */
+        redisTemplate.opsForValue().increment(TOTAL_LIKES + key);
         return redisTemplate.opsForSet().add(LIKE + key, value.toString());
     }
 
@@ -76,15 +94,25 @@ public class BookDetailService implements BookDetailRepository {
     public Long setHate(Long key, Long value) {
         /* 좋아요에 대한 기록을 지워줌 */
         redisTemplate.opsForSet().remove(LIKE + key.toString(), value.toString());
-
+        /* 총 좋아요 개수 -1, 단 좋아요의 개수가 0 미만으로 가면 안됨. */
+        if (Integer.parseInt(Objects.requireNonNull(redisTemplate.opsForValue().get(TOTAL_LIKES + key)).toString()) > 0) {
+            redisTemplate.opsForValue().decrement(TOTAL_LIKES + key);
+        }
         return redisTemplate.opsForSet().add(HATE + key, value.toString());
     }
 
-    @Override
-    public Long undoFeedback(Long key, Long value) {
-        /* 좋아요,싫어요 모든 정보를 지워줌 */
-        redisTemplate.opsForSet().remove(HATE + key.toString(), value.toString());
-        return redisTemplate.opsForSet().remove(LIKE + key, value.toString());
+    public Long undoLike(Long key, Long value) {
+        /* 총 좋아요 개수 -1, 단 좋아요의 개수가 0 미만으로 가면 안됨. */
+        if (Integer.parseInt(Objects.requireNonNull(redisTemplate.opsForValue().get(TOTAL_LIKES + key)).toString()) > 0) {
+            redisTemplate.opsForValue().decrement(TOTAL_LIKES + key);
+        }
+        /* 좋아요 정보를 지워줌 */
+        return redisTemplate.opsForSet().remove(LIKE + key.toString(), value.toString());
+    }
+
+    public Long undoHate(Long key, Long value) {
+        /* 싫어요 정보를 지워줌 */
+        return redisTemplate.opsForSet().remove(HATE + key.toString(), value.toString());
     }
 
     /* 싫어요, 미선택 -> 좋아요를 눌렀던 시점의 점수 계산 및 기록 저장 */
