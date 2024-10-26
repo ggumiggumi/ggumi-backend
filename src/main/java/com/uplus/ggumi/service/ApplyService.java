749439696f9be8f4ec954ade10fd9947e225ea4f
고경남 <rhrudska987@naver.com>
2024-10-26 17:34:00 +0900

    refactor: SecuryConfig 수정
    ## 작업 내용
    origin 추가

--- src/main/java/com/uplus/ggumi/service/ApplyService.java
+++ src/main/java/com/uplus/ggumi/service/ApplyService.java
@@ -1,14 +1,13 @@
 package com.uplus.ggumi.service;
 
-import com.uplus.ggumi.config.exception.ApiException;
-import com.uplus.ggumi.config.exception.ErrorCode;
+import org.springframework.stereotype.Service;
+
 import com.uplus.ggumi.domain.apply.Apply;
-import com.uplus.ggumi.domain.child.Child;
+import com.uplus.ggumi.dto.apply.ApplyRequestDto;
 import com.uplus.ggumi.repository.ApplyRepository;
-import com.uplus.ggumi.repository.ChildRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
@@ -16,19 +15,16 @@ import org.springframework.stereotype.Service;
 public class ApplyService {
 
     private final ApplyRepository applyRepository;
-    private final ChildRepository childRepository;
 
     /* 1단계 기본 Spring Boot + MySQL을 사용한 단일 모듈 구조
      * 응모 요청이 들어오면 MySQL에 바로 save()를 호출해 데이터 저장 */
-    public Boolean applyVer1(Long childId, Long applyTime) {
-
-        Child child = childRepository.findById(childId).orElseThrow(() -> new ApiException(ErrorCode.CHILD_NOT_EXIST));
-
+    public Boolean applyVer1(ApplyRequestDto requestDto) {
         applyRepository.save(Apply.builder()
-                .child(child)
-                .applyTime(applyTime)
+                .name(requestDto.getName())
+                .phoneNumber(requestDto.getPhoneNumber())
+                .applyTime(requestDto.getApplyTime())
                 .build());
-
         return true;
     }
+
 }
