49fddf90c8433ce0d818e2e927def4b0b9d14ae9
heogeonho <geonh1016@gmail.com>
2024-10-31 11:38:05 +0900

    feat: 각 Repository에 findAllByIsDeletedAndDeletedAtBefore() 생성

--- src/main/java/com/uplus/ggumi/repository/FeedbackRepository.java
+++ src/main/java/com/uplus/ggumi/repository/FeedbackRepository.java
@@ -3,6 +3,8 @@ package com.uplus.ggumi.repository;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -19,4 +21,6 @@ public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
 	@Modifying
 	@Query("UPDATE Feedback f SET f.isDeleted = true, f.deletedAt = :now WHERE f.child.id = :childId")
 	int markFeedbackAsDeletedByChildId(@Param("childId") Long childId, @Param("now") LocalDateTime now);
+
+	Page<Feedback> findAllByIsDeletedAndDeletedAtBefore(boolean isDeleted, LocalDateTime deletedAt, Pageable pageable);
 }
