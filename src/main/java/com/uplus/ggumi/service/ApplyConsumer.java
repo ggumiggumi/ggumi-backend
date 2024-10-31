4fe1dbcdf8ce29d0e107c4ce81339b84ec4a42ab
hyoseok <alexization@gmail.com>
2024-10-31 15:12:11 +0900

    feat: 기능 설명을 위한 주석 처리 후 기타 옵션 수정

--- src/main/java/com/uplus/ggumi/service/ApplyConsumer.java
+++ src/main/java/com/uplus/ggumi/service/ApplyConsumer.java
@@ -1,5 +1,6 @@
 package com.uplus.ggumi.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uplus.ggumi.domain.apply.Apply;
 import com.uplus.ggumi.dto.apply.ApplyRequestDto;
 import com.uplus.ggumi.repository.ApplyRepository;
@@ -23,73 +24,77 @@ import java.util.concurrent.CompletableFuture;
 @Slf4j
 public class ApplyConsumer {
 
-    private static final String STREAM_KEY = "apply_stream";
-    private static final String GROUP_NAME = "apply_group";
-    private static final String CONSUMER_NAME = "apply_consumer";
+    /* Redis Stream 키 값으로, 메시지를 저장하고 소비할 Redis Stream 이름 */
+    private static final String APPLY_STREAM = "apply_stream";
+    /* Stream에 대한 Consumer Group의 이름으로 메시지를 여러 Consumer가 그룹으로 처리할 수 있도록 관리 */
+    private static final String APPLY_GROUP = "apply_group";
+    /* Consumer Group 내의 특정 Consumer의 이름으로 각 Consumer가 그룹 내에서 메시지를 읽어서 처리할 수 있도록 함 */
+    private static final String APPLY_CONSUMER = "apply_consumer";
 
     private final ApplyRepository applyRepository;
     private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper objectMapper;
 
+    /* Spring에서 Bean이 생성된 후 실행되는 메서드로 초기화를 위함 */
     @PostConstruct
     public void init() {
         StreamOperations<String, Object, Object> streamOps = redisTemplate.opsForStream();
 
         // 스트림이 존재하지 않으면 첫 메시지를 넣어 스트림을 생성
-        if (Boolean.FALSE.equals(redisTemplate.hasKey(STREAM_KEY))) {
+        if (Boolean.FALSE.equals(redisTemplate.hasKey(APPLY_STREAM))) {
             Map<String, String> initMessage = new HashMap<>();
             initMessage.put("message", "init");
-            streamOps.add(STREAM_KEY, initMessage);
+            streamOps.add(APPLY_STREAM, initMessage);
         }
 
         // 그룹이 존재하지 않으면 그룹을 생성
         try {
-            streamOps.createGroup(STREAM_KEY, GROUP_NAME);
+            streamOps.createGroup(APPLY_STREAM, APPLY_GROUP);
         } catch (Exception e) {
             // 그룹이 이미 존재하면 예외 무시
         }
     }
 
-    @Scheduled(fixedDelay = 1000)  // 2초 간격으로 실행
+    /* 10초 간격으로 pollMessages() 메서드 실행 */
+    @Scheduled(fixedDelay = 10000)
     public void pollMessages() {
         StreamOperations<String, Object, Object> streamOps = redisTemplate.opsForStream();
 
+        /* read() 메서드를 통해 APPLY_GROUP 의 APPLY_CONSUMER 라는 Consumer를 통해 메시지를 읽어온다. */
         List<MapRecord<String, Object, Object>> messages = streamOps.read(
-                Consumer.from(GROUP_NAME, CONSUMER_NAME),
-                StreamReadOptions.empty().block(Duration.ofSeconds(1)),
-                StreamOffset.create("apply_stream", ReadOffset.lastConsumed())
+                Consumer.from(APPLY_GROUP, APPLY_CONSUMER),
+                /* Redis Stream을 최대 0.1초 동안 블로킹해서 메시지를 기다린다. 0.1초 안에 새로운 메시지가 있으면 즉시 읽고, 없으면 0.1초 후 반환한다. */
+                StreamReadOptions.empty().block(Duration.ofMillis(100)),
+                /* Consumer Group의 마지막으로 읽은 메시지 이후의 메시지를 읽어온다. */
+                StreamOffset.create(APPLY_STREAM, ReadOffset.lastConsumed())
         );
 
+        /* 읽어온 메시지가 있으면 processMessage() 메서드 실행 */
         if (messages != null) {
             messages.forEach(this::processMessage);
-        } else {
-            log.info("No new messages in stream.");
         }
+
     }
 
+    /* 메시지 처리 및 MySQL로 저장하기 위한 수행 메서드 */
     private void processMessage(MapRecord<String, Object, Object> record) {
-        ApplyRequestDto requestDto = mapToDto(record.getValue());
+        ApplyRequestDto requestDto = objectMapper.convertValue(record.getValue(), ApplyRequestDto.class);
 
+        /* runAsync()를 통해 비동기적으로 메시지를 처리하며 MySQL 데이터베이스에 저장한다. */
         CompletableFuture.runAsync(() -> {
-            Apply applyEntity = new Apply(
+            applyRepository.save(new Apply(
                     requestDto.getName(),
                     requestDto.getPhoneNumber(),
                     requestDto.getApplyTime()
-            );
-            applyRepository.save(applyEntity);
+            ));
 
-            // 처리 완료 후 Ack를 통해 메시지 확인 처리
-            redisTemplate.opsForStream().acknowledge(GROUP_NAME, record);
+            /* 처리 완료 후 Ack를 통해 메시지 확인 처리
+            * 메시지 처리가 완료되면 acknowledge()를 호출해 해당 Consumer Group에서 메시지가 처리되었음을 Redis에 알려준다.
+            * 이를 통해 동일한 메시지를 다시 읽어오는 일이 발생하지 않도록 해준다. */
+            redisTemplate.opsForStream().acknowledge(APPLY_GROUP, record);
         }).exceptionally(e -> {
             log.error("Error saving message to MySQL", e);
             return null;
         });
-    }
-
-    private ApplyRequestDto mapToDto(Map<Object, Object> value) {
-        return new ApplyRequestDto(
-                (String) value.get("name"),
-                (String) value.get("phoneNumber"),
-                Long.parseLong(value.get("applyTime").toString())
-        );
     }
 }
