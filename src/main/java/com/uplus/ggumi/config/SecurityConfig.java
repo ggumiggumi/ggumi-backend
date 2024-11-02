bd8ffde287ccd939aeff21f1d78a17dd7c3dbb90
hyoseok <alexization@gmail.com>
2024-11-02 16:07:52 +0900

    Netlify 연동을 위한 Security 수정

--- src/main/java/com/uplus/ggumi/config/SecurityConfig.java
+++ src/main/java/com/uplus/ggumi/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
 				@Override
 				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 					CorsConfiguration config = new CorsConfiguration();
-					config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001", "http://localhost:8080", "http://13.125.110.207:8080", "https://hyodev.kr"));
+					config.setAllowedOrigins(Arrays.asList("https://ggumi.netlify.app","http://localhost:3000", "http://localhost:3001", "http://localhost:8080", "http://13.125.110.207:8080", "https://hyodev.kr"));
 					config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 					config.setAllowCredentials(true);
 					config.setAllowedHeaders(Arrays.asList("Authorization", "Authorization-refresh", "Cache-Control", "Content-Type"));
